0894f9e317f2b3eddce5ca83fef6096e7b029910
artmoorejr <152712551+artmoorejr@users.noreply.github.com>
2023-12-02 10:20:50 -0500

    Update day1.py
    better commented than previous

--- day1.py
+++ day1.py
@@ -6,19 +6,19 @@ with open('input.txt','r') as f:
     line = f.readline()
     while line != '':
 
-        word_index = len(line)
+        word_index = len(line)  # start with value above range
         for index in range(9):
             found_index = line.find(numbers[index])
-            if found_index != -1 and found_index < word_index:
+            if found_index != -1 and found_index < word_index:  #if number text is found
 
-                word_index = found_index
-                word_value = index
+                word_index = found_index                        #note where it was found in text
+                word_value = index                              #word_value = index in number[], one less than actual value
                 # print(numbers[found_index])
 
 
         for char in range(len(line)-1):
             if line[char].isdigit():
-                if word_index < char and word_index != len(line):
+                if word_index < char and word_index != len(line):  #did text or digit come first?
                     value = (word_value + 1) * 10
                 else:
                     value = int(line[char]) * 10
@@ -26,7 +26,7 @@ with open('input.txt','r') as f:
 
         word_index = -1
         for index in range(9):
-            found_index = line.rfind(numbers[index])
+            found_index = line.rfind(numbers[index])  #find first text word from end of input string
             # print(found_index)
             if found_index != -1 and found_index > word_index:
                 word_index = found_index
@@ -35,7 +35,7 @@ with open('input.txt','r') as f:
 
         for char in range(len(line)-1, -1, -1):
             if line[char].isdigit():
-                if word_index > char and word_index != -1:
+                if word_index > char and word_index != -1:   #see if word or digit came first by checking index of first spotted
                     value += (word_value + 1)
                 else:
                     value += int(line[char])
